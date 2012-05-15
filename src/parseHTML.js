a165d5649fc0c89197c7a76db12aac5e5d3129b7
Peter Michaux <petermichaux@gmail.com>
2012-05-14 22:38:19 -0700

    rewrite placeholder code with real code that works properly with table-related and other tricky elements in IE

--- src/parseHTML.js
+++ src/parseHTML.js
@@ -1,11 +1,73 @@
 (function() {
 
-    var trimLeft = /^\s+/;
-    var trimRight = /\s+$/;
-
+    var trimLeft = /^\s+/,
+        trimRight = /\s+$/;
     function trim(str) {
         return str.replace(trimLeft, '').replace(trimRight, '');
     }
+
+    function getFirstChild(element) {
+        return element.firstChild;
+    }
+    function getFirstGrandChild(element) {
+        return element.firstChild.firstChild;
+    }
+    function getSecondGrandChild(element) {
+        return element.firstChild.firstChild.nextSibling;
+    }
+    function getFirstGreatGrandChild(element) {
+        return element.firstChild.firstChild.firstChild;
+    }
+    function getFirstGreatGreatGrandChild(element) {
+        return element.firstChild.firstChild.firstChild.firstChild;
+    }
+
+    function Parser(before, after, getFirstResult) {
+        if (before) {
+            this.before = before;
+        }
+        if (after) {
+            this.after = after;
+        }
+        if (getFirstResult) {
+            this.getFirstResult = getFirstResult;
+        }
+    };
+    Parser.prototype = {
+        before: '',
+        after: '',
+        parse: function(html, doc) {
+            var parser = doc.createElement('div');
+            var fragment = doc.createDocumentFragment();
+            parser.innerHTML = this.before + html + this.after;
+            // console.log(parser.innerHTML);
+            var node = this.getFirstResult(parser);
+            var nextNode;
+            while (node) {
+                nextNode = node.nextSibling;
+                fragment.appendChild(node);
+                node = nextNode;
+            }
+            return fragment;
+        },
+        getFirstResult: getFirstChild
+    };
+
+    var parsers = {
+        'td': new Parser('<table><tbody><tr>', '</tr></tbody></table>', getFirstGreatGreatGrandChild),
+        'tr': new Parser('<table><tbody>', '</tbody></table>', getFirstGreatGrandChild),
+        'tbody': new Parser('<table>', '</table>', getFirstGrandChild),
+        'col': new Parser('<table><colgroup>', '</colgroup></table>', getFirstGreatGrandChild),
+        // Without the option in the next line, the parsed option will always be selected.
+        'option': new Parser('<select><option>a</option>', '</select>', getSecondGrandChild)
+    };
+    parsers.th = parsers.td;
+    parsers.thead = parsers.tbody;
+    parsers.tfoot = parsers.tbody;
+    parsers.caption = parsers.tbody;
+    parsers.colgroup = parsers.tbody;
+
+    var tagRegExp = /^<([a-z]+)/i; // first group must be tag name
 
 /**
 
@@ -14,6 +76,8 @@
 @parameter html {string} The string of HTML to be parsed.
 
 @parameter doc {Document} Optional document object to create the new DOM nodes.
+
+@return {DocumentFragment}
 
 @description
 
@@ -29,17 +93,12 @@ don't want to have a loop of thousands with calls to this function.
 
 */
     abeja.parseHTML = function(html, doc) {
-        // IE will trim when setting innerHTML so unify this behavior
-        // across all browsers by trimming html now.
+        // IE will trim when setting innerHTML so unify for all browsers
         html = trim(html);
-        doc = doc || document;
-        var parser = doc.createElement('div');
-        var fragment = doc.createDocumentFragment();
-        parser.innerHTML = html;
-        while (parser.firstChild) {
-            fragment.appendChild(parser.firstChild);
-        }
-        return fragment;
+        var matches = html.match(tagRegExp),
+            parser = (matches && parsers[matches[1].toLowerCase()]) ||
+                     Parser.prototype;
+        return parser.parse(html, doc || document);
     };
 
 }());
--- tst/parseHTMLSuite.js
+++ tst/parseHTMLSuite.js
@@ -11,7 +11,137 @@ buster.testCase("parseHTML test suite", {
         var html = '   <p>'+innerHTML+'</p>   ';
         var frag = abeja.parseHTML(html);
         assert.same('P',  frag.firstChild.tagName);
-        assert.same('a paragraph', frag.firstChild.innerHTML);
+        assert.same(innerHTML, frag.firstChild.innerHTML);
+    },
+
+    "test parse list item": function() {
+        var innerHTML = 'a list item';
+        var html = '   <li>'+innerHTML+'</li>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('LI',  frag.firstChild.tagName);
+        assert.same(innerHTML, frag.firstChild.innerHTML);
+    },
+
+    "test parse td": function() {
+        var innerHTML = 'a table cell';
+        var html = '   <td>'+innerHTML+'</td>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('TD',  frag.firstChild.tagName);
+        assert.same(innerHTML, frag.firstChild.innerHTML);
+    },
+
+    "test parse th": function() {
+        var innerHTML = 'a table cell';
+        var html = '   <th>'+innerHTML+'</th>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('TH',  frag.firstChild.tagName);
+        assert.same(innerHTML, frag.firstChild.innerHTML);
+    },
+
+    "test parse tr": function() {
+        var innerHTML = '<td>a table cell</td>';
+        var html = '   <tr>'+innerHTML+'</th>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('TR',  frag.firstChild.tagName);
+        assert.same('TD',  frag.firstChild.firstChild.tagName);
+    },
+
+    "test parse thead": function() {
+        var html = '   <thead></thead>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('THEAD',  frag.firstChild.tagName);
+    },
+
+    "test parse tbody": function() {
+        var html = '   <tbody></tbody>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('TBODY',  frag.firstChild.tagName);
+    },
+
+    "test parse tfoot": function() {
+        var html = '   <tfoot></tfoot>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('TFOOT',  frag.firstChild.tagName);
+    },
+
+    "test parse caption": function() {
+        var html = '   <caption>a caption</caption>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('CAPTION',  frag.firstChild.tagName);
+    },
+
+    "test parse col": function() {
+        var html = '   <col>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('COL',  frag.firstChild.tagName);
+    },
+
+    "test parse colgroup": function() {
+        var html = '   <colgroup></colgroup>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('COLGROUP',  frag.firstChild.tagName);
+    },
+
+    "test parse select": function() {
+        var html = '   <select></select>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('SELECT',  frag.firstChild.tagName);
+    },
+
+    "test parse unselected option": function() {
+        var html = '   <option></option>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('OPTION',  frag.firstChild.tagName);
+        assert.same(false, frag.firstChild.selected);
+    },
+
+    "test parse selected option": function() {
+        var html = '   <option selected="selected"></option>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('OPTION',  frag.firstChild.tagName);
+        assert.same(true, frag.firstChild.selected);
+    },
+
+    "test parse unchecked radio": function() {
+        var html = '   <input type="radio" name="alpha"></option>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('INPUT',  frag.firstChild.tagName);
+        assert.same(false, frag.firstChild.checked);
+    },
+
+    "test parse checked radio": function() {
+        var html = '   <input type="radio" name="alpha" checked="checked"></option>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('INPUT',  frag.firstChild.tagName);
+        assert.same(true, frag.firstChild.checked);
+    },
+
+    "test parse fieldset": function() {
+        var html = '   <fieldset></fieldset>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('FIELDSET',  frag.firstChild.tagName);
+    },
+
+    "test parse legend": function() {
+        var html = '   <legend>a legend</legend>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('LEGEND',  frag.firstChild.tagName);
+    },
+
+    "test parse text": function() {
+        var text = 'abc';
+        var html = '   '+text+'   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('"'+text+'"',  '"'+frag.firstChild.nodeValue+'"');
+    },
+
+    "test parse multiple elements": function() {
+        var text = 'abc';
+        var html = '   <div>asdf</div>'+text+'<span>asdf</span>   ';
+        var frag = abeja.parseHTML(html);
+        assert.same('DIV',  frag.firstChild.tagName);
+        assert.same('"'+text+'"',  '"'+frag.firstChild.nextSibling.nodeValue+'"');
+        assert.same('SPAN',  frag.firstChild.nextSibling.nextSibling.tagName);
     }
 
 });
